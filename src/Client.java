f6b5cd5cec91810692fb23609932e4790ec675d6
Camille Bouquet <bouquetcamille@hotmail.fr>
2011-11-30 11:43:03 +0100

    Ajout dun message derreur pour meux voir ce qui se passe

--- src/Client.java
+++ src/Client.java
@@ -2,13 +2,14 @@ import java.rmi.*;
 
 public class Client {
 	public static void main(String args[]) {
+		System.out.println("Client lancé");
 		try {
-			System.out.println("Client lancé");
 			// Récupération d'un stub sur l'objet serveur.
 			Message obj = (Message) Naming.lookup("//127.0.0.1/mon_serveur");
 			// Appel d'une méthode sur l'objet distant.
 			obj.sayHello();
 		} catch (Exception exc) {
+			System.err.println("Erreur dans la communication");
 		}
 	}
 }
