573e29c29b92b2d0f8c6ee40a3c9671e9a85d8f6
Antoine Sellam <sphoenixx@gmail.com>
2011-12-09 10:44:42 +0100

    Ajout du diagramme de classes

--- src/Client.java
+++ src/Client.java
@@ -4,6 +4,7 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.*;
 
 public class Client {
 	public static void main(String args[]) {
