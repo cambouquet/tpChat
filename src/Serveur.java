30871e73ac622b8bf88fed216e7bcc0df3607bf9
Antoine Sellam <sphoenixx@gmail.com>
2012-01-12 09:10:50 +0100

    Creation de connect() dans le serveur

--- src/Serveur.java
+++ src/Serveur.java
@@ -3,6 +3,7 @@ import java.rmi.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Serveur extends UnicastRemoteObject implements SetClientThread {
@@ -17,6 +18,8 @@ public class Serveur extends UnicastRemoteObject implements SetClientThread {
 	private Registry registry;
 	private String inetAdresse;
 	private int setPort;
+	private HashMap<Integer,String> listeClients;
+	private static int lastClientID = 1336;
 
 	// Implémentation du constructeur
 	public Serveur() throws java.rmi.RemoteException {
@@ -56,9 +59,21 @@ public class Serveur extends UnicastRemoteObject implements SetClientThread {
 	}
 
 	// Implémentation de la méthode distante
-	public int connect() throws java.rmi.RemoteException {
-		// TODO
-		return 0;
+	public int connect(String clientNick) throws java.rmi.RemoteException {
+		//Vérification du pseudo
+		if (listeClients.containsValue(clientNick)) {
+			String clientNickOld = clientNick;			
+			clientNick = clientNick + "_";
+			while (listeClients.containsValue(clientNick)) {
+				clientNick = clientNick + "_";
+			}
+			System.out.println("Pseudo " + clientNickOld + " déjà utilisé. Vous êtes enregistré en tant que " + clientNick);
+		}
+		
+		//Attribution d'un clientID
+		int currentClientID = lastClientID + 1;
+		listeClients.put(currentClientID, clientNick);
+		return currentClientID;
 	}
 	
 	public Message send()
