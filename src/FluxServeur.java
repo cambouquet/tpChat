164dc43514764d524b8012d9f4aa274e71172bcc
Antoine Sellam <sphoenixx@gmail.com>
2012-01-12 08:29:28 +0100

    Ajout de l interface FluxServeur

---
+++ src/FluxServeur.java
@@ -0,0 +1,10 @@
+import java.rmi.Remote;
+import java.util.LinkedList;
+
+public interface FluxServeur extends Remote {
+	
+	public void envMessage(String sc) throws java.rmi.RemoteException;
+	
+	public LinkedList<Message> syncServ(int lastMessage) throws java.rmi.RemoteException;
+	
+}
